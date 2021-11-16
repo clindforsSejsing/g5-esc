fdc6f57cf1cc0406d41e000757e2655468182bf5
CLS <clindforsSejsing@gmail.com>
2021-11-16 10:59:27 +0100

    modal rewritten, close-btn gone, tags written in js

--- src/modules/booking.js
+++ src/modules/booking.js
@@ -1,11 +1,45 @@
 "strict coding";
 
+
+// function cardCtaBtnClick(title, minP, maxP) {
+
+//   const modal = document.getElementById("myModal");
+//   modal.style.display = "block";
+//   const infoCard = (document.getElementById(
+//     "bookThis"
+//   ).innerHTML = `Book Room ${title} (Step 1)`);
+
+//   // user clicks outside modal, close modal
+//   window.addEventListener("click", function (event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   });
+//   //close modal when user clicks(x)
+//   document.querySelector(".close").addEventListener("click", () => {
+//     modal.style.display = "none";
+//   });
+// }
+
+export { cardCtaBtnClick };
+/*---------------*/
+
+
 function cardCtaBtnClick(title, minP, maxP) {
+
   const modal = document.getElementById("myModal");
   modal.style.display = "block";
+
+  /*create modal*/
   const infoCard = (document.getElementById(
     "bookThis"
   ).innerHTML = `Book Room ${title} (Step 1)`);
+  (document.getElementById("whatDate")).innerHTML = "What date would you like to come";
+  (document.getElementById("dateText")).innerHTML = "Date";
+  (document.getElementById("date")).placeholder = "yyyy-mm-dd";
+  (document.getElementById("searchTimes-btn")).innerHTML = "Search available times";
+
+
 
   // user clicks outside modal, close modal
   window.addEventListener("click", function (event) {
@@ -13,10 +47,4 @@ function cardCtaBtnClick(title, minP, maxP) {
       modal.style.display = "none";
     }
   });
-  //close modal when user clicks(x)
-  document.querySelector(".close").addEventListener("click", () => {
-    modal.style.display = "none";
-  });
 }
-
-export { cardCtaBtnClick };
