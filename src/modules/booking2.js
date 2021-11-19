f83d478e5f293050c3cb578f9d9a867a1f6adea0
CLS <clindforsSejsing@gmail.com>
2021-11-19 15:50:37 +0100

    merging min with modalbranch

--- src/modules/booking2.js
+++ src/modules/booking2.js
@@ -1,116 +1,91 @@
 "strict coding";
 
 let btn = document.querySelector("button");
-const modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
+const modalwrap = document.getElementById("modal-content");
+
 btn.addEventListener("click", function () {
     firstModal();
-
-
-
-    // const modal = document.getElementById("myModal");
-    // modal.style.display = "block";
-
-    // //create tags in modal
-    // const newSpan = document.createElement("span");
-    // newSpan.setAttribute("id", "close");
-    // document.getElementById("myModal").append(newSpan);
-    // newSpan.innerHTML = "&times";
-    // const titelModal = document.createElement("h1");
-    // titelModal.setAttribute("id", "bookThis");
-    // document.getElementById("myModal").append(titelModal);
-    // const text1Modal = document.createElement("p");
-    // text1Modal.setAttribute("id", "whatDate");
-    // document.getElementById("myModal").append(text1Modal);
-    // const text2Modal = document.createElement("p");
-    // text2Modal.setAttribute("id", "dateText");
-    // document.getElementById("myModal").append(text2Modal);
-    // const inputField1 = document.createElement("input");
-    // inputField1.setAttribute("id", "date");
-    // inputField1.setAttribute("type", "text");
-    // document.getElementById("myModal").append(inputField1);
-    // const searchBtn = document.createElement("button");
-    // searchBtn.setAttribute("id", "searchTimes-btn");
-    // document.getElementById("myModal").append(searchBtn);
-
-
-
-
-    // document.getElementById("close").innerHTML = "&times";
-
-    //fill tags with text
-
-    // document.getElementById("whatDate").innerHTML = "What date would you like to come";
-    // document.getElementById("dateText").innerHTML = "Date";
-    // document.getElementById("date").placeholder = "yyyy-mm-dd";
-    // document.getElementById("searchTimes-btn").innerHTML = "Search available times";
-
-    //close modal when user clicks(x)
-    // document.querySelector(".close").addEventListener("click", () => {
-    //     modal.style.display = "none";
-    // });
-    // user clicks outside modal, close modal
-    // window.addEventListener("click", function (event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
+    modalValue();
+   
 });
-
+//create tags and set value//
 function firstModal() {
-    const newSpan = document.createElement("span");
-    newSpan.setAttribute("id", "close");
-    document.getElementById("myModal").append(newSpan);
-    newSpan.innerHTML = "&times";
-    const titelModal = document.createElement("h1");
-    titelModal.setAttribute("id", "bookThis");
-    document.getElementById("myModal").append(titelModal);
-    const text1Modal = document.createElement("p");
-    text1Modal.setAttribute("id", "whatDate");
-    document.getElementById("myModal").append(text1Modal);
-    const text2Modal = document.createElement("p");
-    text2Modal.setAttribute("id", "dateText");
-    document.getElementById("myModal").append(text2Modal);
-    const inputField1 = document.createElement("input");
-    inputField1.setAttribute("id", "date");
-    inputField1.setAttribute("type", "text");
-    document.getElementById("myModal").append(inputField1);
-    const searchBtn = document.createElement("button");
-    searchBtn.setAttribute("id", "searchTimes-btn");
-    document.getElementById("myModal").append(searchBtn);
-    //give modal.-tags value
-    document.getElementById("whatDate").innerHTML = "What date would you like to come";
-    document.getElementById("dateText").innerHTML = "Date";
-    document.getElementById("date").placeholder = "yyyy-mm-dd";
-    document.getElementById("searchTimes-btn").innerHTML = "Search available times";
-    //click på searchbtn
-    searchBtn.addEventListener("click", function () {
-        // window.alert("helloo");
-        modal.innerHTML = " " + secondModal;
+const titelModal = document.createElement("h1");
+titelModal.setAttribute("id", "bookThis");
+modalwrap.append(titelModal);
+const text1Modal = document.createElement("p");
+text1Modal.setAttribute("id", "whatDate");
+modalwrap.append(text1Modal);
+const text2Modal = document.createElement("p");
+text2Modal.setAttribute("id", "dateText");
+modalwrap.append(text2Modal);
+const inputField1 = document.createElement("input");
+inputField1.setAttribute("id", "date");
+inputField1.setAttribute("type", "text");
+modalwrap.append(inputField1);
+const searchBtn = document.createElement("button");
+searchBtn.setAttribute("id", "searchTimes-btn");
+modalwrap.append(searchBtn);
 
-        //create form
-
-        function secondModal() {
-            const newForm = document.createElement("form");
-            newForm.setAttribute("id", "contactForm");
-            document.getElementById("myModal").append(myForm);
-
-            const newSpan = document.createElement("span");
-            newSpan.setAttribute("id", "close");
-            document.getElementById("myModal").append(newSpan);
-            newSpan.innerHTML = "&times";
-        }
-
-    });
+  
+    //click on searchbtn
+    searchBtn.addEventListener("click", function (){
+        //get date
+         let userDate = document.getElementById("date").value;
+       getTimeSlots(userDate);
+       //create form
+       secondModal();
+       
+     });
 }
 
-// let searchTimesBtn = document.getElementById("searchTimes-btn");
+// give modal.-tags value
+function modalValue(){
+document.getElementById("whatDate").innerHTML = "What date would you like to come";
+document.getElementById("dateText").innerHTML = "Date";
+document.getElementById("date").placeholder = "yyyy-mm-dd";
+document.getElementById("searchTimes-btn").innerHTML = "Search available times";
+}
 
 
-// searchBtn.addEventListener("click", function () {
-//     window.alert("helloo");
+//inhämta data
+async function getTimeSlots(wantedDate)
+{
+  const response = await fetch ('https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=' + wantedDate);
+  const slotJson = await response.json();
+//   window.alert(slotJson.slots);
+}
 
-//     //create form
+function secondModal() {
+    modal.style.display = "block";
+    emptyModal();
+//create form
+    const newForm = document.createElement("form");
+    newForm.setAttribute("id", "contactForm");
+    modalwrap.append(newForm);
+//Behöver hitta var att appenda. Modalen syns men modelwrap finns nog inte. ev sätta in den i form
+    const nameInputFieldText = document.createElement("p");
+    nameInputFieldText.setAttribute("id","nameText");
+    document.getElementById("nameText").innerHTML = "Name";
+    modalwrap.append(nameInputFieldText);
 
-// });
+    const nameinputField = document.createElement("input");
+    nameinputField.setAttribute("id", "nameInput");
+    nameinputField.setAttribute("type", "text");
+    modalForm.append(nameinputField);
+
+    const emailinputField = document.createElement("input");
+    emailinputField.setAttribute("id", "email");
+    emailinputField.setAttribute("type", "text");
+    document.getElementById("email").innerHTML = "Email";
+    modalForm.append(emailinputField);
+}
+function emptyModal() {
+    if (modalwrap.innertext != ""){
+        (modalwrap.innerText = "");
+    }      
+};
 
     //     const formName = document.getElementById("whatDate").innerHTML = "Name";
 
@@ -129,3 +104,7 @@ function firstModal() {
 
     // });
 
+        // const newSpan = document.createElement("span");
+    // newSpan.setAttribute("id", "close");
+    // document.getElementById("modalwrap").append(newSpan);
+    // newSpan.innerHTML = "&times";
